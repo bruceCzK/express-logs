2025f226e99361f361a53b76e4a94fc6f4595e00
叫我布鲁斯 <bruceczk@foxmail.com>
2016-09-19 09:56:59 +0800

    Remove getLogs method; Add requestTiming option (default on);

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
  * Created by chenzhuokai on 16/7/25.
  */
 const LogStack = require('./lib/log-stack');
-module.exports = function (req, res, next) {
-  req.logs = new LogStack();
+module.exports = function (options) {
+  return function (req, res, next) {
+    req.logs = new LogStack(options);
 
-  res.on('finish', function () {
-    req.logs.output();
-  });
+    res.on('finish', function () {
+      req.logs.output();
+    });
 
-  next();
+    next();
+  };
 };
